4dabc6be802409042d7c2011ad33b0497a20e439
Chathura Yapa <chathura.yapa@solita.fi>
2023-07-11 08:39:42 +0300

    Added Pytest base test configuration

---
+++ flight_declaration_operations/test_views.py
@@ -0,0 +1,23 @@
+import pytest
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+  
+
+
+JWT = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ0ZXN0ZmxpZ2h0LmZsaWdodGJsZW5kZXIuY29tIiwiY2xpZW50X2lkIjoidXNzX25vYXV0aCIsImV4cCI6MTY4Nzc4Mjk0OCwiaXNzIjoiTm9BdXRoIiwianRpIjoiODI0OWI5ODgtZjlkZi00YmNhLWI2YTctODVhZGFiZjFhMTUwIiwibmJmIjoxNjg3Nzc5MzQ3LCJzY29wZSI6ImJsZW5kZXIucmVhZCIsInN1YiI6InVzc19ub2F1dGgifQ.b63qZWs08Cp1cgfRCtbQfLom6QQyFpqUaFDNZ9ZdAjSM690StACij6FiriSFhOfFiRBv9rE0DePJzElUSwv1r1bI0IpKMtEJYsJY4DXy7ZImiJ3rSten1nnb1LLAELcDIxMZM2D1ek43EFW35al4si640JfMcSmt62bEP1b4Msc"
+
+class FlightDeclarationTests(APITestCase):
+
+    def test_incorrect_payload(self):
+       # client.credentials(HTTP_AUTHORIZATION=f'Bearer {JWT}')
+
+        url = reverse("set_flight_declaration")
+        incorrect_data =99
+
+        response = self.client.post(url, incorrect_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+
+        assert True == True
+
+
