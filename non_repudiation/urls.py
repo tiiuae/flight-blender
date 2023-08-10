b8c6636d3dd22b00de3513316d788ee8b8297aec
Chathura Yapa <chathura.yapa@solita.fi>
2023-08-10 14:46:42 +0300

    Added unit tests for creating new public keys

--- non_repudiation/urls.py
+++ non_repudiation/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("public_keys/", views.PublicKeyList.as_view()),
-    path("public_keys/<uuid:pk>/", views.PublicKeyDetail.as_view()),
+    path("public_keys/", views.PublicKeyList.as_view(), name="public_keys"),
+    path("public_keys/<uuid:pk>/", views.PublicKeyDetail.as_view(), name="public_keys"),
 ]
