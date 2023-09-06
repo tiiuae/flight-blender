efeee425229474ad57c93fc83746252a7cfd1163
Chathura Yapa <chathura.yapa@solita.fi>
2023-09-06 15:11:05 +0300

    Added base serializers

--- flight_feed_operations/serializers.py
+++ flight_feed_operations/serializers.py
@@ -8,3 +8,16 @@ class SignedTelmetryPublicKeySerializer(serializers.ModelSerializer):
         model = SignedTelmetryPublicKey        
         fields = ('key_id','url', 'is_active',)
    
+
+
+class CurrentStateSerializer(serializers.Serializer):
+    # Define fields for the "current_states" object if needed
+    pass
+
+class FlightDetailsSerializer(serializers.Serializer):
+    # Define fields for the "flight_details" object if needed
+    pass
+
+class ObservationSerializer(serializers.Serializer):
+    current_states = CurrentStateSerializer(many=True)
+    flight_details = FlightDetailsSerializer()
