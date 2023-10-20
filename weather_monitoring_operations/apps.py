00203458b2b5ee493dc861cac0cea70936a82992
lfonosol <lance.fono@solita.fi>
2023-10-20 16:06:05 +0300

    Run black on weather_monitoring files

--- weather_monitoring_operations/apps.py
+++ weather_monitoring_operations/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class WeatherMonitoringOperationsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'weather_monitoring_operations'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "weather_monitoring_operations"
