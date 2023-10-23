591620118e95ff14133d941a18bcd8d7e1586c5a
lfonosol <lance.fono@solita.fi>
2023-10-23 16:15:53 +0300

    refactor test

--- api_clients/weather_data/meteo.py
+++ api_clients/weather_data/meteo.py
@@ -1,9 +1,9 @@
 from api_clients.api_client import ApiClient
 
+
 class MeteoApiClient(ApiClient):
     def __init__(self):
         pass
-        
+
     def get_data():
         return ""
-
--- api_clients/weather_data/test_weather_data_clients.py
+++ api_clients/weather_data/test_weather_data_clients.py
@@ -1,10 +1,11 @@
-
 from django.test import TestCase
 
 from api_clients.weather_data.meteo import MeteoApiClient
 
+
 class MeteoApiClientTestCase(TestCase):
+    def setUp(self):
+        self.client = MeteoApiClient()
+
     def test_meteo_api_client(self):
-        meteoClient = MeteoApiClient()
-        
-        self.assertIsInstance(meteoClient, MeteoApiClient)
+        self.assertIsInstance(self.client, MeteoApiClient)
