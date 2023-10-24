c76dace8e22119b2f754cc82318a1c330366b397
lfonosol <lance.fono@solita.fi>
2023-10-25 02:07:21 +0300

    add hourly attributes to url

--- api_clients/weather_data/meteo.py
+++ api_clients/weather_data/meteo.py
@@ -5,32 +5,46 @@ from location_vector import LocationVector
 
 class MeteoApiClient(ApiClient):
     def __init__(self):
-        self.api_base_url = "https://api.open-meteo.com/v1/forecast" 
+        self.api_base_url = "https://api.open-meteo.com/v1/forecast"
         self.available_attrs = ["weathercode", "temperature_2m"]
 
-    def get_data(self, location_vector=None, weather_attrs=["weathercode", "temperature_2m"]):
+    def get_data(
+        self, location_vector=None, weather_attrs=["weathercode", "temperature_2m"]
+    ):
         self._check_location_vector(location_vector)
         self._check_weather_attrs(weather_attrs)
 
         return json.dumps({attr: attr for attr in weather_attrs})
-    
-    def get_api_url(self, location_vector=None):
+
+    def get_api_url(self, location_vector=None, weather_attrs=None):
         self._check_location_vector(location_vector)
-        location_vector_query = self._get_location_vector_query_params(location_vector) 
-        return "https://api.open-meteo.com/v1/forecast?" + location_vector_query
-    
+        self._check_weather_attrs(weather_attrs)
+        location_vector_query = self._get_location_vector_query_params(location_vector)
+        weather_attrs_query = self._get_weather_attrs_query_params(weather_attrs)
+        return (
+            "https://api.open-meteo.com/v1/forecast?"
+            + location_vector_query
+            + weather_attrs_query
+        )
+
     def _check_location_vector(self, location_vector):
         if not isinstance(location_vector, LocationVector):
             raise ValueError("Invalid location vector")
-        
+
         for attr in location_vector:
             if not isinstance(attr, (float, int)):
                 raise ValueError("Invalid location vector")
 
     def _check_weather_attrs(self, weather_attrs):
+        if weather_attrs is None:
+            raise ValueError("Weather attributes are required")
+
         for attr in weather_attrs:
             if attr not in self.available_attrs:
                 raise ValueError(f"Invalid weather attribute: {attr}")
-            
+
     def _get_location_vector_query_params(self, location_vector):
         return f"longitude={location_vector.longitude}&latitude={location_vector.latitude}&elevation={location_vector.elevation}"
+
+    def _get_weather_attrs_query_params(self, weather_attrs):
+        return "&hourly=" + ",".join(weather_attrs)
--- api_clients/weather_data/test_weather_data_clients.py
+++ api_clients/weather_data/test_weather_data_clients.py
@@ -13,24 +13,26 @@ class MeteoApiClientTestCase(TestCase):
         self.valid_location_vector = LocationVector(24.4512, 54.397, 2)
 
         self.assertTupleEqual(self.valid_location_vector, (24.4512, 54.397, 2))
-        self.assertIsNotNone(self._parse_data_fail_on_exception("{\"test\": \"test\"}"))
+        self.assertIsNotNone(self._parse_data_fail_on_exception('{"test": "test"}'))
         self.assertRaises(Exception, self._parse_data_fail_on_exception, "random")
 
     def test_meteo_api_client_with_missing_location_vector(self):
         location_vector = None
         self.assertIsInstance(self.client, MeteoApiClient)
         self.assertRaises(ValueError, self.client.get_data, location_vector)
-        
+
     def test_meteo_api_client_with_invalid_location_vector(self):
         invalid_location_vector = LocationVector(24.4512, 54.397, "w")
         self.assertRaises(ValueError, self.client.get_data, invalid_location_vector)
-        
+
     def test_meteo_api_client_with_location_vector(self):
-        data = self.client.get_data(self.valid_location_vector, ["weathercode", "temperature_2m"])
+        data = self.client.get_data(
+            self.valid_location_vector, ["weathercode", "temperature_2m"]
+        )
         parsed_data = self._parse_data_fail_on_exception(data)
 
         self.assertIsNotNone(parsed_data.get("weathercode"))
-        self.assertIsNotNone(parsed_data.get("temperature_2m"))        
+        self.assertIsNotNone(parsed_data.get("temperature_2m"))
 
     def test_meteo_api_client_get_data(self):
         data = self.client.get_data(self.valid_location_vector)
@@ -42,23 +44,36 @@ class MeteoApiClientTestCase(TestCase):
     def test_meteo_api_client_get_data_with_query_params(self):
         self.assertIsInstance(self.client, MeteoApiClient)
 
-        data = self.client.get_data(self.valid_location_vector, ["weathercode", "temperature_2m"])
+        data = self.client.get_data(
+            self.valid_location_vector, ["weathercode", "temperature_2m"]
+        )
         parsed_data = self._parse_data_fail_on_exception(data)
 
         self.assertIsNotNone(parsed_data.get("weathercode"))
         self.assertIsNotNone(parsed_data.get("temperature_2m"))
         self.assertIsNone(parsed_data.get("random"))
         self.assertRaises(ValueError, self.client.get_data, ["random"])
-        
-    def test_meteo_api_client_get_api_url(self):
-        
-        url = self.client.get_api_url(location_vector=self.valid_location_vector)
+
+    def test_meteo_api_client_get_api_url_with_location_vector(self):
+        self.assertRaises(
+            ValueError,
+            self.client.get_api_url,
+            location_vector=self.valid_location_vector,
+        )
+
+        url = self.client.get_api_url(
+            location_vector=self.valid_location_vector,
+            weather_attrs=["weathercode", "temperature_2m"],
+        )
+
         parsed_url = urlparse(url)
         query_params = parse_qs(parsed_url.query)
-        
+
         self.assertEqual(query_params, query_params | {"latitude": ["24.4512"]})
         self.assertEqual(query_params, query_params | {"longitude": ["54.397"]})
         self.assertEqual(query_params, query_params | {"elevation": ["2"]})
+
+        self.assertEqual("hourly=weathercode,temperature_2m" in url, True)
 
     def _parse_data_fail_on_exception(self, data):
         try:
