3a3cc43a661dda4edbda17676f8e00a581b51090
lfonosol <lance.fono@solita.fi>
2023-10-24 17:27:06 +0300

    Add location vector as required param

--- api_clients/weather_data/meteo.py
+++ api_clients/weather_data/meteo.py
@@ -1,19 +1,27 @@
 import json
 from api_clients.api_client import ApiClient
-
+from location_vector import LocationVector
 
 
 class MeteoApiClient(ApiClient):
     def __init__(self):
         self.available_attrs = ["weathercode", "temperature_2m"]
 
-    def get_data(self,weather_attrs=["weathercode", "temperature_2m"]):
-        
-        self.check_weather_attrs(weather_attrs)          
+    def get_data(self, location_vector=None, weather_attrs=["weathercode", "temperature_2m"]):
+        self._check_location_vector(location_vector)
+        self._check_weather_attrs(weather_attrs)
 
-        return json.dumps({attr: attr for attr in weather_attrs})        
-                 
-    def check_weather_attrs(self, weather_attrs):
+        return json.dumps({attr: attr for attr in weather_attrs})
+    
+    def _check_location_vector(self, location_vector):
+        if not isinstance(location_vector, LocationVector):
+            raise ValueError("Invalid location vector")
+        
+        for attr in location_vector:
+            if not isinstance(attr, (float, int)):
+                raise ValueError("Invalid location vector")
+
+    def _check_weather_attrs(self, weather_attrs):
         for attr in weather_attrs:
             if attr not in self.available_attrs:
                 raise ValueError(f"Invalid weather attribute: {attr}")
--- api_clients/weather_data/test_weather_data_clients.py
+++ api_clients/weather_data/test_weather_data_clients.py
@@ -2,37 +2,51 @@ import json
 from django.test import TestCase
 
 from api_clients.weather_data.meteo import MeteoApiClient
+from location_vector import LocationVector
 
 
 class MeteoApiClientTestCase(TestCase):
     def setUp(self):
         self.client = MeteoApiClient()
+        self.valid_location_vector = LocationVector(24.4512, 54.397, 2)
+        self.assertIsNotNone(self._parse_data_fail_on_exception("{\"test\": \"test\"}"))
+        self.assertRaises(Exception, self._parse_data_fail_on_exception, "random")
 
-    def test_meteo_api_client(self):
-        
+    def test_meteo_api_client_with_location_vector(self):
+        location_vector = LocationVector(24.4512, 54.397, "w")
+        self.assertRaises(ValueError, self.client.get_data, location_vector)
+
+        self.assertTupleEqual(self.valid_location_vector, (24.4512, 54.397, 2))
+
+        data = self.client.get_data(self.valid_location_vector, ["weathercode", "temperature_2m"])
+        parsed_data = self._parse_data_fail_on_exception(data)
+
+        self.assertIsNotNone(parsed_data.get("weathercode"))
+        self.assertIsNotNone(parsed_data.get("temperature_2m"))
+
+    def test_meteo_api_client_get_data(self):
         self.assertIsInstance(self.client, MeteoApiClient)
-        
-        data = self.client.get_data()
+        self.assertRaises(ValueError, self.client.get_data, None)
+
+        data = self.client.get_data(self.valid_location_vector)
         self.assertIsNotNone(data)
-        
+
         parsed_data = self._parse_data_fail_on_exception(data)
         self.assertIsInstance(parsed_data, dict)
-        
-    def test_meteo_api_client_with_query_params(self):
-        self.client = MeteoApiClient()    
-        
+
+    def test_meteo_api_client_get_data_with_query_params(self):
         self.assertIsInstance(self.client, MeteoApiClient)
-        
-        data = self.client.get_data(["weathercode", "temperature_2m"])
+
+        data = self.client.get_data(self.valid_location_vector, ["weathercode", "temperature_2m"])
         parsed_data = self._parse_data_fail_on_exception(data)
-        
+
         self.assertIsNotNone(parsed_data.get("weathercode"))
         self.assertIsNotNone(parsed_data.get("temperature_2m"))
         self.assertIsNone(parsed_data.get("random"))
-            
+        self.assertRaises(ValueError, self.client.get_data, ["random"])
+
     def _parse_data_fail_on_exception(self, data):
         try:
             return json.loads(data)
         except:
             self.fail("Data is not valid JSON")
-    
